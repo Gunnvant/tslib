e587ec9621859e575e48bc43190743b46cd4405b
gunnvant <gunnvant3.14@gmail.com>
2025-02-04 14:33:50 +0530

    ENH: Time delay embedding support and example included

--- tslib/preprocessing.py
+++ tslib/preprocessing.py
@@ -1,5 +1,7 @@
 from tslib.interface import Preprocessing
 from tslib.exceptions import NoCVDataSetPossible
+import numpy as np
+import pandas as pd
 
 class CVPreprocessor(Preprocessing):
     def __init__(self,start_idx,step_size,horizon):
@@ -20,3 +22,31 @@ class CVPreprocessor(Preprocessing):
                 start_idx+=self.step_size
                 folds.append({'train':train,'test':test})
         return folds
+
+class TimeDelayEmbedding(Preprocessing):
+    '''
+    Ref: Cerqueira, Vitor, Nuno Moniz, and Carlos Soares. "VEST: Automatic Feature Engineering for Forecasting." arXiv preprint arXiv:2010.07137 (2020).
+    https://github.com/vcerqueira/vest-python
+    '''
+    def __init__(self,ts:pd.Series,k=5):
+        self.ts=ts 
+        self.k=k 
+    
+    def fit(self):
+        X,y = [],[]
+        
+        for i in range(len(self.ts)):
+            # find the end of this pattern
+            end_ix = i + self.k
+            # check if we are beyond the sequence
+            if end_ix > len(self.ts) - 1:
+                break
+            # gather input and output parts of the pattern
+            seq_x, seq_y = self.ts.values[i:end_ix], self.ts.values[end_ix]
+            X.append(seq_x)
+            y.append(seq_y)
+
+        X = np.array(X)
+        y = np.array(y)
+
+        return X, y
